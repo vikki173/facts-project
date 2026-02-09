123afb5c0bb3d26f7726a3673c06c506a4cabb73
vikas_r_b <vikasrokkad@gmail.com>
2026-02-09 11:01:22 +0530

    Initial facts project

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+facts = [
+    {"id":1, "fact":"The Sun is hot."},
+    {"id":2, "fact":"The moon is beautiful."},
+    {"id":3, "fact":"I am Intelligent."},
+    {"id":4, "fact":"I know Python."},
+    {"id":5, "fact":"I am deciplined."},
+    {"id":6, "fact":"earth has one moon."},
+]
+
+@app.get("/api/facts")
+def get_facts():
+    return facts
